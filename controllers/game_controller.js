8dde93a942a7155f583ec3d90e025a4be20c5a32
William Young <williamcorinyoung@gmail.com>
2018-05-05 13:37:03 -0700

    YOUNG introducing controllers

---
+++ controllers/game_controller.js
@@ -0,0 +1,36 @@
+var express = require("express");
+
+// var character = require("../models/characters.js");
+
+// var spaceship = require("../models/spaceships.js");
+
+var db = require("../models");
+
+
+var router = express.Router();
+
+
+//post route uses the character model to create our character with the users choice of name
+router.post("/api/character", function (req, res) {
+	db.character.create(req.body.name, function (result) {
+		res.json({ id: result.insertId });
+	});
+});
+//post route creates our spaceship in database with chosen name
+router.post("/api/spaceship", function (req, res) {
+	db.spaceship.create(req.body.name, function (result) {
+		res.json({ id: result.insertId });
+	});
+});
+//put route updates certain values pertaining to character based on events ex. when damage is taken, health value is adjusted in DB
+router.put("/api/character/:id", function (req, res) {
+	var condition = "id = " + req.params.id;
+});
+//put route updates certain values pertaining to character based on events ex. when damage is taken, health value is adjusted in DB
+router.put("/api/spaceship/:id", function (req, res) {
+	var condition = "id = " + req.params.id;
+
+});
+
+// Export routes for server.js to use.
+module.exports = router;
