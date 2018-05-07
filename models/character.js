a33b048b792a4dcd7f38335d2264fe0e5505546e
William Young <williamcorinyoung@gmail.com>
2018-05-06 19:14:46 -0700

    merge

--- models/character.js
+++ models/character.js
@@ -1,4 +1,4 @@
-module.exports = function(sequelize, DataTypes){
+module.exports = function (sequelize, DataTypes) {
     var character = sequelize.define("character", {
         name: {
             type: DataTypes.STRING,
