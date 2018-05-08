34262a86e6d72d6647fd883905bb1f35881ae6ef
killianmh <killianmh@gmail.com>
2018-05-07 22:11:19 -0700

    killian get correct tutorial error message

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var db = require("./models");
-
+var bodyParser = require('body-parser');
 var express = require('express');
 var passport = require('passport')
 var session = require('express-session')
-var bodyParser = require('body-parser');
 var env = require('dotenv').load();
 
 var app = express();
@@ -28,7 +27,9 @@ var routes = require("./controllers/game_controller.js");
 
 app.use(routes);
 
-db.sequelize.sync({}).then(function () {
+require('./config/passport/passport.js')(passport, db.user);
+
+db.sequelize.sync({force: true}).then(function () {
 	app.get('/', function (req, res) {
 
 		res.send('Welcome to Passport with Sequelize');
