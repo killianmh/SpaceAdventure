df25491fa50a12c21b913bf9b4e954f59a814442
killianmh <killianmh@gmail.com>
2018-05-13 19:51:56 -0700

    killian deploy to heroku

--- models/character.js
+++ models/character.js
@@ -3,7 +3,7 @@ module.exports = function (sequelize, DataTypes) {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true,
+            // unique: true,
             validate: {
                 isAlphanumeric: true
             }
