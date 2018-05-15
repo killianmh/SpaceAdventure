6faac1ca882ea47f173993384d75f4b36ad40da0
William Young <williamcorinyoung@gmail.com>
2018-05-14 17:06:52 -0700

    login text input failing

--- test/nightmare-login.js
+++ test/nightmare-login.js
@@ -5,6 +5,9 @@ var nightmare = Nightmare({ show: true });
 nightmare
 	.goto("https://stark-meadow-67785.herokuapp.com/")
 	.click("#login-modal")
+	.click("a[href='#home']")
+	.type("input[name='username'", "testUser")
+	.type("input[name='password'", "1234")
 	.wait(30000)
 	.type("#search_form_input_homepage", "github nightmare")
 	.wait("#links a")
